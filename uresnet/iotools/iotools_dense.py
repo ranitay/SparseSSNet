dense network fix, loss, masking

uresnet/iotools/iotools_dense.py
@@ -53,8 +53,8 @@ MainIO: {
     %s
     %s
     %s
-    NumThreads: 2
-    NumBatchStorage: 2
+    NumThreads: 1
+    NumBatchStorage: 1
     ProcessList: {
        %s
     }
@@ -196,6 +196,8 @@ class io_larcv_dense(io_base):
                 blob[key] = np.array(np.swapaxes(np.swapaxes(np.swapaxes(data,4,3),3,2),2,1))
             else:
                 blob[key] = np.array(np.swapaxes(np.swapaxes(data,3,2),2,1))
+       # Ran ITay Change here to threshold
+        blob = self.Applythreshold(blob)
         idx = np.array(self._ihandler.fetch_entries())
         self._next_counter += 1
         return idx, blob
@@ -204,3 +206,18 @@ class io_larcv_dense(io_base):
         self._ihandler.reset()
         if self._fout:
             self._fout.finalize()
+
+    def Applythreshold(self, orig_blob):
+        blob = {}
+        for key in orig_blob.keys():
+            blob[key] = []
+        for i,image in enumerate(orig_blob['wire']):
+            mask = np.where((image<=10)|(image>=300)|(image!=0))
+            for key in orig_blob.keys():
+                blob[key].append(orig_blob[key][i])
+                if (key == 'label'):
+                    blob[key][i][mask] = 5
+                else:
+                    blob[key][i][mask] = 0 # label 0 is proton so label 5 is no ADC pixel
+        return blob
+

uresnet/models/uresnet_sparse.py
@@ -69,7 +69,7 @@ class SegmentationLoss(torch.nn.modules.loss._Loss):
                 if weight is not None:
                     event_weight = weight[i][batch_index]
                     event_weight = torch.squeeze(event_weight, dim=-1).float()
-		    total_loss += torch.sum(loss_seg * event_weight) # RanItay change this
+                    total_loss += torch.sum(loss_seg * event_weight) # RanItay change this
 #                   total_loss += torch.mean(loss_seg * event_weight)
                 else:
                     total_loss += torch.mean(loss_seg)

uresnet/trainval.py
@@ -62,39 +62,40 @@ class trainval(object):
 
             for key in data_blob.keys():
                 blob[key] = data_blob[key][idx]
-
-            data  = blob['data'][idx]
-            if 'label' in blob:
-                label = blob['label'][idx]
-            if 'weights' in blob:
-                weight = blob['weights'][idx]
-            #            if (idx % 10000 ==0):
-            #	    	print('number of points = %s and number of points removed for low Thresh is %s, number of points removed above high thresh is %s' %
-            #                    (np.sum(data[:, 3]>=0), np.sum(data[:, 3] <= 10),np.sum(data[:,3]>=300)))
-            N_Thresh = np.sum(np.logical_and(data[:, 3] >= 10 ,data[:,3]<=300))
-            i_Thresh = 0
-            thresh_data  = np.zeros( (N_Thresh,data.shape[1]),  dtype=data.dtype  )
-            if 'label' in blob:
-                thresh_label  = np.zeros( (N_Thresh,label.shape[1]),  dtype=label.dtype  )
-            if 'weights' in blob:
-                thresh_weight  = np.zeros( (N_Thresh,weight.shape[1]),  dtype=weight.dtype  )
-
-            for i in range(data.shape[0]):
-                if(np.logical_and(data[i,3]>=10 ,data[i,3]<=300)):
-                    thresh_data[i_Thresh,:] =data[i,:]
-                    if 'label' in blob:
-                        thresh_label[i_Thresh,:] = label[i,:]
-                    if 'weights' in blob:
-                        thresh_weight[i_Thresh,:] = label[i,:]
-
-                    i_Thresh +=1
-
-            blob['data'][idx]  = thresh_data
-            if 'label' in blob:
-                blob['label'][idx]  = thresh_label
-            if 'weights' in blob:
-                blob['weights'][idx]  = thresh_weight
-
+            '''    
+            if self._flags.MODEL_NAME==uresnet_sparse:
+                data  = blob['data'][idx]
+                if 'label' in blob:
+                    label = blob['label'][idx]
+                if 'weights' in blob:
+                    weight = blob['weights'][idx]
+                #            if (idx % 10000 ==0):
+                #	    	print('number of points = %s and number of points removed for low Thresh is %s, number of points removed above high thresh is %s' %
+                #                    (np.sum(data[:, 3]>=0), np.sum(data[:, 3] <= 10),np.sum(data[:,3]>=300)))
+                N_Thresh = np.sum(np.logical_and(data[:, 3] >= 10 ,data[:,3]<=300))
+                i_Thresh = 0
+                thresh_data  = np.zeros( (N_Thresh,data.shape[1]),  dtype=data.dtype  )
+                if 'label' in blob:
+                    thresh_label  = np.zeros( (N_Thresh,label.shape[1]),  dtype=label.dtype  )
+                if 'weights' in blob:
+                    thresh_weight  = np.zeros( (N_Thresh,weight.shape[1]),  dtype=weight.dtype  )
+    
+                for i in range(data.shape[0]):
+                    if(np.logical_and(data[i,3]>=10 ,data[i,3]<=300)):
+                        thresh_data[i_Thresh,:] =data[i,:]
+                        if 'label' in blob:
+                            thresh_label[i_Thresh,:] = label[i,:]
+                        if 'weights' in blob:
+                            thresh_weight[i_Thresh,:] = label[i,:]
+    
+                        i_Thresh +=1
+    
+                blob['data'][idx]  = thresh_data
+                if 'label' in blob:
+                    blob['label'][idx]  = thresh_label
+                if 'weights' in blob:
+                    blob['weights'][idx]  = thresh_weight
+            '''
 
 
 
@@ -129,11 +130,12 @@ class trainval(object):
         # matplotlib.image.imsave('label1.png', label[1, 0, ...])
         with torch.set_grad_enabled(self._flags.TRAIN):
             # Segmentation
+#            data = torch.as_tensor(data)
             data = [torch.as_tensor(d) for d in data]
             if torch.cuda.is_available():
                 data = [d.cuda() for d in data]
             else:
-                data = data[0]
+               data = data[0]
             tstart = time.time()
             segmentation = self._net(data)
             if not torch.cuda.is_available():